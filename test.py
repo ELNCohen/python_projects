f6676f27d6cfc9f037151babff91f2e0af228f19
ELNCohen <ethan.ln.cohen@gmail.com>
2018-10-16 23:15:47 -0400

    test atom git plugin

---
+++ test.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+import sklearn
+
+df = pd.DataFrame([[1,2,3],[1,2,3],[3,2,1]],columns=['a','b','c'])
+
+print(df)
+
+
+print(dfsdmksdm)
+
+df.a.hist()
