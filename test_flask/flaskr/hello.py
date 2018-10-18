25c141d7658732cffb597ea97f307fa8f15f2a3a
ELNCohen <ethan.ln.cohen@gmail.com>
2018-10-18 15:25:10 -0400

    flask tutorial testing

---
+++ test_flask/flaskr/hello.py
@@ -0,0 +1,80 @@
+from flask import Flask, redirect, url_for, \
+    request, render_template, make_response
+
+app = Flask(__name__)
+
+
+# @app.route('/')
+# def student():
+#     return render_template('student.html')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/setcookie', methods=['POST', 'GET'])
+def setcookie():
+    if request.method == 'POST':
+        user = request.form['nm']
+
+    resp = make_response(render_template('readcookie.html'))
+    resp.set_cookie('userID', user)
+
+    return resp
+
+
+@app.route('/getcookie')
+def getcookie():
+    name = request.cookies.get('userID')
+    return '<h1>welcome '+name+'</h1>'
+
+
+@app.route('/result', methods=['POST', 'GET'])
+def result():
+    if request.method == 'POST':
+        result = request.form
+        return render_template("result.html", result=result)
+
+
+@app.route('/hello/<int:score>')
+def hello_name(score):
+    return render_template('hello.html', marks=score)
+
+
+@app.route('/admin')
+def hello_admin():
+    return 'Hello Admin'
+
+
+@app.route('/guest/<guest>')
+def hello_guest(guest):
+    return 'Hello %s as Guest' % guest
+
+
+@app.route('/user/<name>')
+def hello_user(name):
+    if name == 'admin':
+        return redirect(url_for('hello_admin'))
+    else:
+        return redirect(url_for('hello_guest', guest=name))
+
+
+@app.route('/success/<name>')
+def success(name):
+    return 'welcome %s' % name
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['nm']
+        return redirect(url_for('success', name=user))
+    else:
+        user = request.args.get('nm')
+        return redirect(url_for('success', name=user))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
